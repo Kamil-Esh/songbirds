0ac6d6ba83ff656cf0f7a57457998c54fe47fdb8
Kamil <eshimbekov91@gmail.com>
2021-02-21 22:37:07 +0400

    'initial'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import "./App.css"
+import BirdsApp from "./components/BirdsApp/BirdsApp";
+const App = () => {
+    return (
+        <div>
+            <BirdsApp />
+        </div>
+)
 }
 
 export default App;
